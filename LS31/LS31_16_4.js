103028575eb0b664bdd54973078b8a7d4d71c7a0
liuzixue1998 <825673952@qq.com>
2018-06-25 16:32:37 +0800

    week17 Monday

--- LS31/LS31_16_4.js
+++ LS31/LS31_16_4.js
@@ -243,14 +243,15 @@ s.has(2); // false
 var properties = new Set();
 properties.add('width');
 properties.add('height');
-console.log(properties.size);
+console.log(properties.size);//2
 if (properties.has('width')&&properties.has('height')) {
     console.log("do something!");
-}
+}//do something!
 
 //Array.from方法可以将Set结构转为数组。
 var items = new Set([1, 2, 3, 4, 5]);
 var array = Array.from(items);
+console.log(array);// (5) [1, 2, 3, 4, 5]
 
 //...也可以将set转换成数组
 console.log([...(new Set([1, 2, 3, 4, 5]))]);//[1, 2, 3, 4, 5]
@@ -260,9 +261,9 @@ console.log(...(new Set([1, 2, 3, 4, 5])));//1, 2, 3, 4, 5
 
 //关于Set的遍历方法
 var set = new Set(['red', 'green', 'blue']);
-console.log(typeof set.keys());//注意是什么类型，是否可迭代，是否可用for...of遍历
-console.log(typeof set.values());
-console.log(typeof set.entries());
+console.log(typeof set.keys());//object
+console.log(typeof set.values());//object
+console.log(typeof set.entries());//object
 
 //keys方法、values方法、entries方法返回的都是遍历器对象
 for (var item of set.keys()) {
@@ -288,6 +289,10 @@ for (var item of set.entries()) {
 for (var [key,value] of set.entries()) {
     console.log(key,value);
 }
+// ["red", "red"]
+// ["green", "green"]
+// ["blue", "blue"]
+
 
 //Set结构的实例的forEach方法，用于对每个成员执行某种操作，没有返回值。
 var set = new Set([1, 2, 3]);
@@ -297,17 +302,21 @@ set.forEach((value, key) => console.log(value * 2) );
 // 6
 
 //而且，数组的map和filter方法也可以间接用于Set了，通过...转成数组后调用后再生成set
+//map()方法返回一个新数组，数组中的元素为原始数组元素调用函数处理的后值。 
+//map()方法按照原始数组元素顺序依次处理元素。
+//map不会对空数组进行检测，不会改变原数组
 var set = new Set([1, 2, 3]);
 set = new Set([...set].map(x => x * 2));
 // 返回Set结构：{2, 4, 6}
+//filter() 方法创建一个新的数组，新数组中的元素是通过检查指定数组中符合条件的所有元素。
 var set = new Set([1, 2, 3, 4, 5]);
 set = new Set([...set].filter(x => (x % 2) == 0));
 // 返回Set结构：{2, 4}
 
 
 // set应用案例 并集、交集
-let a = new Set([1, 2, 3]);
-let b = new Set([4, 3, 2]);
+    let a = new Set([1, 2, 3]);
+    let b = new Set([4, 3, 2]);
 // 并集
 let union = new Set([...a, ...b]);
 // Set {1, 2, 3, 4}
@@ -334,3 +343,251 @@ WeakSet 结构与 Set 类似，也是不重复的值的集合。但是，它与 
 运行前后很可能成员个数是不一样的，而垃圾回收机制何时运行是不可预测的，
 因此 ES6 规定 WeakSet 不可遍历。
 */
+
+
+/*
+3.新增数据结构Map
+- 它类似于对象，也是键值对的集合，但是“键”的范围不限于字符串
+- Object结构提供了“字符串-值”的对应，Map结构提供了“值-值”的对应
+- 创建Map时也可以使用一个数组作为构造参数，此数组的每个元素是键值对的数组
+*/
+var m = new Map();
+var o = {p: 'Hello World'};
+console.log(o);//{p: "Hello World"}
+console.log(m);//Map(1) {{…} => "content"}
+m.set(o, 'content');
+m.get(o); // "content"
+m.has(o); // true
+m.delete(o); // true
+m.has(o); // false
+
+//
+//作为构造函数，Map也可以接受一个数组作为参数。该数组的成员是一个个表示键值对的数组。
+var o = {};
+var map = new Map([
+    ['name', '张三'],
+    [o, 'Author']
+]);
+map.size; // 2
+map.has('name'); // true
+map.get('name'); // "张三"
+map.has(o); // true
+map.get(o); // "Author"
+
+//Map构造函数接受数组作为参数，实际上执行的是下面的算法。
+var items = [
+    ['name', '张三'],
+    ['title', 'Author']
+];
+var map = new Map();
+items.forEach(([key, value]) => map.set(key, value));
+//等效于
+// items.forEach(function(v){
+// 	map.set(v[0],v[1]);
+// });
+
+//
+// 如果对同一个键多次赋值，后面的值将覆盖前面的值。
+let map = new Map();
+map.set(1, 'aaa').set(1, 'bbb');
+map.get(1); // "bbb"
+// 上面代码对键1连续赋值两次，后一次的值覆盖前一次的值。
+// 如果读取一个未知的键，则返回undefined。
+new Map().get('asfddfsasadf');
+// undefined
+
+//只有对同一个对象的引用，Map结构才将其视为同一个键。这一点要非常小心。
+//思考对象会存在这个问题么，对象的键是什么类型，判等是依据什么？
+//要特别注意
+var map = new Map();
+map.set(['a'], 555);
+map.get(['a']); // undefined
+
+//思考：
+var map = new Map();
+map.set('a', 555);
+map.get('a'); //555
+
+//
+var map = new Map();
+var k1 = ['a'];
+var k2 = ['a'];
+map.set(k1, 111);
+map.set(k2, 222);
+map.get(k1); // 111
+map.get(k2); // 222
+//上面代码中，变量k1和k2的值是一样的，但是它们在Map结构中被视为两个键。
+// 由上可知，Map的键实际上是跟内存地址绑定的，只要内存地址不一样，就视为两个键。
+// 这就解决了同名属性碰撞（clash）的问题，我们扩展别人的库的时候，如果使用对象作为键名，
+// 就不用担心自己的属性与原作者的属性同名。
+
+// 如果Map的键是一个简单类型的值（数字、字符串、布尔值），则只要两个值严格相等，Map将其视为一个键，
+// 包括0和-0。另外，虽然NaN不严格相等于自身，但Map将其视为同一个键（特例）。
+let map = new Map();
+map.set(NaN, 123);
+map.get(NaN); // 123
+map.set(-0, 123);
+map.get(+0); // 123
+let map = new Map();
+map.set(undefined, 123);
+map.get(undefined); // 123
+
+
+/*
+3(1).Map的原型属性和方法
+- Map.prototype.size、Map.prototype.set(key、value)、Map.prototype.get(key)
+- Map.prototype.has(key)、Map.prototype.delete(key)、Map.prototype.clear( )
+- Map.prototype.keys( )（注意返回类型）、Map.prototype.values( )、Map.prototype.entries( )
+*/
+let map = new Map();
+map.set('foo', true);
+map.set('bar', false);
+map.size // 2
+map;//Map(2) {"foo" => true, "bar" => false}
+
+var m = new Map();
+m.set("edition", 6); // 键是字符串
+m.set(262, "standard"); // 键是数值
+m.set(undefined, "nah"); // 键是undefined
+
+// set方法返回的是Map本身，因此可以采用链式写法
+let map = new Map()
+    .set(1, 'a')
+    .set(2, 'b')
+    .set(3, 'c');
+// get方法读取key对应的键值，如果找不到key，返回undefined。
+var m = new Map();
+var hello = function() {console.log("hello");};
+m.set(hello, "Hello ES6!"); // 键是函数
+m.get(hello); // Hello ES6!
+
+//has方法返回一个布尔值，表示某个键是否在Map数据结构中。
+var m = new Map();
+m.set("edition", 6);
+m.set(262, "standard");
+m.set(undefined, "nah");
+m.has("edition"); // true
+m.has("years"); // false
+m.has(262); // true
+m.has(undefined); // true
+m;//Map(3) {"edition" => 6, 262 => "standard", undefined => "nah"}
+
+//delete方法删除某个键，返回true。如果删除失败，返回false。
+var m = new Map();
+m.set(undefined, "nah");
+m.has(undefined); // true
+m.delete(undefined);
+m.has(undefined); // false
+
+//clear方法清除所有成员，没有返回值。
+let map = new Map();
+map.set('foo', true);
+map.set('bar', false);
+map.size // 2
+map.clear();
+map.size // 0
+
+//Map遍历相关的方法////////////////////////////////
+let map = new Map([
+    ['F', 'no'],
+    ['T', 'yes'],
+]);
+console.log(typeof map.keys());//注意类型是对象，不是数组
+for (let key of map.keys()) {
+    console.log(key);
+}
+// "F"
+// "T"
+console.log(typeof map.values());//注意类型是对象，不是数组
+for (let value of map.values()) {
+    console.log(value);
+}
+// "no"
+// "yes"
+for (let item of map.entries()) {
+    console.log(item[0], item[1]);
+}
+// "F" "no"
+// "T" "yes"
+// 或者 回顾解构赋值
+for (let [key, value] of map.entries()) {
+    console.log(key, value);
+}
+// 等同于使用map.entries()
+// for (let [key, value] of map) {
+//     console.log(key, value);
+// }
+
+/////////////////////////////////////////////
+//Map结构转为数组结构，比较快速的方法是结合使用扩展运算符（...）。
+let map = new Map([
+    [1, 'one'],
+    [2, 'two'],
+    [3, 'three'],
+]);
+console.log([...map.keys()]);
+// [1, 2, 3]
+console.log([...map.values()]);
+// ['one', 'two', 'three']
+console.log([...map.entries()]);
+// [[1,'one'], [2, 'two'], [3, 'three']]
+console.log([...map]);
+// [[1,'one'], [2, 'two'], [3, 'three']]
+
+//结合数组的map方法、filter方法，可以实现Map的遍历和过滤（Map本身没有map和filter方法）。
+let map0 = new Map()
+    .set(1, 'a')
+    .set(2, 'b')
+    .set(3, 'c');
+let map1 = new Map(
+    [...map0].filter(([k, v]) => k < 3)
+);
+// 产生Map结构 {1 => 'a', 2 => 'b'}
+let map2 = new Map(
+    [...map0].map(([k, v]) => [k * 2, '_' + v])
+);
+// 产生Map结构 {2 => '_a', 4 => '_b', 6 => '_c'}
+
+//此外，Map还有一个forEach方法，与数组的forEach方法类似，也可以实现遍历。
+map.forEach(function(value, key, map) {
+    console.log("Key: %s, Value: %s", key, value);
+});
+
+
+
+//扩展内容，补充思考：
+//Map与其他数据结构对象的转换，参考电子书13.3.4章节
+//Map转为数组
+//前面已经提过，Map转为数组最方便的方法，就是使用扩展运算符（...）。
+let myMap = new Map().set(true, 7).set({foo: 3}, ['abc']);
+[...myMap]
+// [ [ true, 7 ], [ { foo: 3 }, [ 'abc' ] ] ]
+
+//数组转为Map
+//将数组转入Map构造函数，就可以转为Map。
+new Map([[true, 7], [{foo: 3}, ['abc']]]);
+// Map {true => 7, Object {foo: 3} => ['abc']}
+
+//Map转为对象
+//如果所有Map的键都是字符串，它可以转为对象。
+function strMapToObj(strMap) {
+    let obj = Object.create(null);
+    for (let [k,v] of strMap) {
+        obj[k] = v;
+    }
+    return obj;
+}
+let myMap = new Map().set('yes', true).set('no', false);
+strMapToObj(myMap)
+// { yes: true, no: false }
+
+//对象转为Map
+function objToStrMap(obj) {
+    let strMap = new Map();
+    for (let k of Object.keys(obj)) {
+        strMap.set(k, obj[k]);
+    }
+    return strMap;
+}
+objToStrMap({yes: true, no: false});
+// [ [ 'yes', true ], [ 'no', false ] ]
